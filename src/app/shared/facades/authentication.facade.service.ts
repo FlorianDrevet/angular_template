eaa057a7ed82720bf0d72e63a9edeb849a7865ae
Florian <florian.drevet@epita.fr>
2024-10-15 19:49:18 +0200

    feat(AuthenticationFacade): add facade to authenticate

---
+++ src/app/shared/facades/authentication.facade.service.ts
@@ -0,0 +1,15 @@
+import {inject, Injectable} from '@angular/core';
+import {AxiosService} from "../services/axios.service";
+import {MethodEnum} from "../enums/method.enum";
+import {AuthTokenInterface} from "../interfaces/authToken.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationFacadeService {
+  axiosService = inject(AxiosService);
+
+  public postLogIn$(username: string, password: string): Promise<AuthTokenInterface> {
+    return this.axiosService.request(MethodEnum.POST, `/auth/login`, {email: username, password});
+  }
+}
