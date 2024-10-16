8b630e98fbaa72623ce97228caca4ee28ce260c5
Florian <florian.drevet@epita.fr>
2024-10-16 08:47:36 +0200

    fox(modules): import FeatureModule in app Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import {NgModule, provideExperimentalZonelessChangeDetection} from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {CoreModule} from "./core/core.module";
+import {FeatureModule} from "./feature/feature.module";
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import {CoreModule} from "./core/core.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    FeatureModule
   ],
   providers: [
     provideAnimationsAsync(),
